63f2bde74794a30bf9bdc54ee23b7e3b2d3c94cd
Makizero <108859150+Makizero@users.noreply.github.com>
2023-12-16 17:11:51 +0300

    Update shikimori-404.js

--- shikimori-404.js
+++ shikimori-404.js
@@ -1,16 +1,1 @@
-var func = function() {
-  const error404Element = document.querySelector("p.error-404");
-  if (error404Element) {
-    const url = window.location.href;
-    if (url.includes('/mangas/')) {
-      const mangaId = url.match(/mangas\/[a-z]*(\d+)-/)[1];
-      const newUrl = `https://myanimelist.net/manga/${mangaId}`;
-      window.location.href = newUrl;
-    } else if (url.includes('/animes/')) {
-      const animeId = url.match(/animes\/[a-z]*(\d+)-/)[1];
-      const newUrl = `https://myanimelist.net/anime/${animeId}`;
-      window.location.href = newUrl;
-    }
-  }
-};
-func();
+var func=функция( ){let e= document.querySelector("p.error -404");if(e){пусть a=окно.местоположение.href;if(a.включает(quot;/mangas/quot;)< /span>=< /span>/< /span>)aаниме*< /span>; l= href.местоположение.окно ;`i${`https://myanimelist.net/anime/ =l,] 1[)/- )+\d(]a-z[\//(соответствует.=ilet{)"/animes/quot;(включает.a(ifиначеn=href.местоположение.окно;`t${ `https://myanimelist.net/manga/=n,] 1[)/- )+\d(*] a-z[\/манга(совпадение.atlet{)   
